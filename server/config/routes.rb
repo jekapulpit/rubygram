12e106c5e0260b059565ffc553a444f6527d6c47
Yauhen <q010@bk.ru>
2019-07-09 12:01:55 +0300

    added crud routes for rooms and messages

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v4 do
+      resources :rooms, only: %i[show index create destroy]
+      resources :messages
+      post '/rooms/unreaded/:id', to: 'rooms#read_all'
+    end
+  end
+
+  mount ActionCable.server => '/cable'
+
 end
