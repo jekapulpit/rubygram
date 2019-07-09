a90d711d0cbaec33ac4460595d2314ad0cc74a80
Yauhen <q010@bk.ru>
2019-07-09 14:02:17 +0300

    added auth services

---
+++ client/src/services/authentificationService.js
@@ -0,0 +1,29 @@
+import { setUserSession, deleteUserSession } from './sessionStorageServices'
+import { setCookie, deleteCookie } from './cookieServices'
+
+export function authentificateUser(userCredintials) {
+    let url = 'http://localhost:3001/api/v4/auth';
+    let requestOpts = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userCredintials)
+    };
+    fetch(url, requestOpts)
+        .then((response) => {return response.json()})
+        .then((data) => {
+            if(data.token) {
+                setUserSession(data.current_user);
+                setCookie('auth_token', data.token);
+            }
+            else {
+                console.log('nope')
+            }
+        })
+}
+
+export function logout() {
+    deleteUserSession();
+    deleteCookie('auth_token');
+}
