8a815c38d9eaec28f7931194a767a37b33aac958
Yauhen <q010@bk.ru>
2019-07-09 11:52:29 +0300

    added rooms controller

---
+++ server/app/controllers/api/v4/rooms_controller.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+class Api::V4::RoomsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def show
+    begin
+      room = Room.includes(:messages, :users).find(params[:id])
+      messages = room.messages
+      render json: { room: room, messages: messages, users: room.users }
+    rescue ArgumentError
+      render json: { error: "error message" }
+    end
+  end
+
+  def index
+    rooms = current_user.rooms
+    render json: { rooms: rooms }
+  end
+
+  def create
+    room = Room.new(room_params)
+    current_user.rooms << room
+    render json: { room: room, success: room.valid? }
+  end
+
+  def destroy
+    room = Room.find(params[:id])
+    render json: { destroyed: room.destroy }
+  end
+
+  def read_all
+    room_relation = RoomRelation.find_by(room_id: params[:id], user: current_user)
+    room_relation&.update(unreaded_number: 0)
+    render json: { success: true }
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name)
+  end
+end
