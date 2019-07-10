4864ff42dfdb5c4a175ce6f3d0ad3b013bb5716d
Yauhen <q010@bk.ru>
2019-07-10 17:55:55 +0300

    added state to an empty slot

--- client/src/components/rooms/EmptySlot.jsx
+++ client/src/components/rooms/EmptySlot.jsx
@@ -1,13 +1,42 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import '../../stylesheets/components/rooms.scss'
+import {mapFieldsToValues} from "../../services/mapFieldsToValuesService";
 
-const EmptySlot = props => {
-    return (
-        <div className='room-card'>
+class EmptySlot extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            editable: false
+        };
+        this.newRoomAttributes = {}
+    }
 
-        </div>
-    )
-};
+    handleNew = () => {
+        this.setState({
+            editable: true
+        })
+    };
+
+    handleCreate = (newRoomAttributes) => {
+
+    };
+
+    render() {
+        let fill = this.state.editable ? (
+            <div>
+                <input ref={input => this.newRoomAttributes.name = input} type="text"/>
+                <button onClick={() => this.handleCreate(mapFieldsToValues(this.newRoomAttributes))}>create</button>
+            </div>
+        ) : (
+            <button onClick={() => this.handleNew()}>+</button>
+        );
+        return (
+            <div className='room-card'>
+                {fill}
+            </div>
+        )
+    }
+}
 
 export default hot(EmptySlot);
