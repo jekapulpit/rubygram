ebe4b26af4f7eb8dd9712e026a790cde725fbc4c
Yauhen <q010@bk.ru>
2019-07-12 14:13:13 +0300

    added services for messages

---
+++ client/src/services/messagesServices.js
@@ -0,0 +1,18 @@
+import { API_HOST, API_PORT, HEADERS } from '../constants'
+import { getTokenFromSessionStorage } from "./sessionStorageServices";
+
+export async function sendMessage(messageAttributes) {
+    return fetch(`http://${API_HOST}:${API_PORT}/api/v4/messages/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': getTokenFromSessionStorage()
+        },
+        body: JSON.stringify(messageAttributes)
+    })
+        .then((response) => { return response.json() });
+}
+
+export async function receiveMessage(message) {
+    console.log(message);
+}
