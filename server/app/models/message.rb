dd6f7f4c2d1abd4ec8718244d48ebe2d8b3d8192
Yauhen <q010@bk.ru>
2019-07-17 11:40:27 +0300

    added es indexes for message

--- server/app/models/message.rb
+++ server/app/models/message.rb
@@ -3,11 +3,21 @@
 class Message < ApplicationRecord
   belongs_to :sender, class_name: 'User', foreign_key: 'sender_id', polymorphic: true
   belongs_to :recipient, class_name: 'Room', foreign_key: 'recipient_id', polymorphic: true
+  searchkick text_middle: [:content]
+
+  scope :search_import, -> { includes(:sender) }
 
   def with_send_info
     attributes.merge({
       senders_name: sender.username,
       send_time: created_at.strftime("%B %d, %Y")
     })
+  end
+
+  def search_data
+    {
+        content: content,
+        sender: sender.username
+    }
   end
 end
