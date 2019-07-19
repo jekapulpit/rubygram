3ce8e9634fa1c8a3032e36de48172229f33f7863
Yauhen <q010@bk.ru>
2019-07-19 14:26:44 +0300

    added seeds for generating default config

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+DefaultSetting.create(setting_type: 'max_chats', value: 5)
+DefaultSetting.create(setting_type: 'max_users', value: 5)
