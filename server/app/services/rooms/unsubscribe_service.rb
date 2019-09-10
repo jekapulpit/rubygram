e438be4dc4245b9bba24ac1a448b379ee10b8268
Yauhen <q010@bk.ru>
2019-09-10 17:00:41 +0300

    added left notifications

--- server/app/services/rooms/unsubscribe_service.rb
+++ server/app/services/rooms/unsubscribe_service.rb
@@ -11,6 +11,16 @@ module Rooms
       relation = RoomRelation.find_by(room: room, user: user)
       invite = Invite.find_by(room: room, user: user)
       invite.destroy if invite
+      message = Message.new(
+          content: "user #{user.username} left the conversation",
+          sender: room,
+          recipient: room
+      )
+      message.save
+      RoomsChannel.broadcast_to room, {
+          message: message,
+          type: 'RECEIVE_MESSAGE'
+      }
       relation.destroy
     end
   end
