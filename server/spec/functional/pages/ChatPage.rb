2d6b8f3a0c00c8dd21d067369aac966bdc3f22ec
Yauhen <q010@bk.ru>
2019-10-04 15:40:25 +0300

    fixed and refactoder functional tests

--- server/spec/functional/pages/ChatPage.rb
+++ server/spec/functional/pages/ChatPage.rb
@@ -1,13 +1,11 @@
 class ChatPage
-  URL = 'http://localhost:3000/home/rooms'
-
   attr_accessor :message_field, :send_button
 
   def initialize(room_id, browser = Watir::Browser.new)
     @browser = browser
+    visit_room(room_id)
     @message_field = @browser.text_field(:id, 'message-field')
     @send_button = @browser.button(:id, 'send-message-btn')
-    visit_room(room_id)
   end
 
   def method_missing(sym, *args, &block)
@@ -15,11 +13,14 @@ class ChatPage
   end
 
   def visit_room(room_id)
-    @browser.goto "#{URL}/#{room_id}"
+    @browser.a(:href, '/home/rooms').click
+    @browser.a(:href, "/home/rooms/#{room_id}").click
+    self
   end
 
   def send_message(content)
     message_field.set(content)
+    sleep 2
     send_button.click
     self
   end
--- server/spec/functional/send_message_spec.rb
+++ server/spec/functional/send_message_spec.rb
@@ -2,32 +2,38 @@
 
 require 'rails_helper'
 
-RSpec.describe 'send message' do
-  let(:room) { Room.first }
-  let(:sender) { room.users.first }
-  let(:receiver) { room.users.last }
+RSpec.describe 'send message', type: :feature do
   let(:test_message_content) { 'test_message' }
 
+  before(:all) do
+    @test_message = 'test_message'
+    @room = FactoryGirl.create(:filled_room, users_count: 1)
+    @sender = @room.users.first
+    @receiver = @room.users.last
+    Support::SignUp.new(browser).call({:email => @sender.email, :password => '123456'})
+    chat_page = ChatPage.new(@room.id, browser)
+    chat_page.send_message(@test_message)
+  end
+
   context 'from senders view' do
-    let(:browser) { Support::SignUp.new.call({:email => sender.email, :password => '123456'}) }
     it 'message should be sent' do
-      browser.a(:href, '/home/rooms').click
-      browser.a(:href, "/home/rooms/#{room.id}").click
-      browser.text_field(:id, 'message-field').set(test_message_content)
-      browser.button(:id, 'send-message-btn').click
       browser.text.should match(/.*#{test_message_content}.*/)
     end
   end
 
   context 'from receivers view' do
-    let(:browser) { Support::SignUp.new.call({:email => sender.email, :password => '123456'}) }
-    it 'message should be sent' do
-      browser.a(:href, '/home/rooms').click
-      browser.a(:href, "/home/rooms/#{room.id}").click
+    before do
+      Support::SignUp.new(browser).call({:email => @receiver.email, :password => '123456'})
+      chat_page = ChatPage.new(@room.id, browser)
+    end
+    it 'message should be received' do
       browser.text.should match(/.*#{test_message_content}.*/)
     end
   end
+
   after(:context) do
     Message.last.destroy
+    Room.destroy_all
+    User.destroy_all
   end
 end
